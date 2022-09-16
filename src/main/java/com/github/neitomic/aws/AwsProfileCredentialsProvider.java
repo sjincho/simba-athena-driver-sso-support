b236856765524184082f0730f17c3d9d3434108d
Seongjin Cho <sjcho@ridi.com>
2022-09-16 15:32:05 +0900

    Adds provider that uses ProfileCredentialsProvider.

---
+++ src/main/java/com/github/neitomic/aws/AwsProfileCredentialsProvider.java
@@ -0,0 +1,49 @@
+package com.github.neitomic.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.simba.athena.amazonaws.auth.BasicAWSCredentials;
+import com.simba.athena.amazonaws.auth.BasicSessionCredentials;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.profiles.ProfileFile;
+
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public class AwsProfileCredentialsProvider implements AWSCredentialsProvider {
+    private final AwsCredentialsProvider credentialsProviderV2;
+
+    public AwsProfileCredentialsProvider(String profilesConfigFilePath, String profileName) {
+        this(ProfileFile.builder().content(Paths.get(profilesConfigFilePath)).type(ProfileFile.Type.CONFIGURATION).build(), profileName);
+    }
+
+    public AwsProfileCredentialsProvider(String profileName) {
+        this(ProfileFile.defaultProfileFile(), profileName);
+    }
+
+    public AwsProfileCredentialsProvider(ProfileFile profileFile, String profileName) {
+        credentialsProviderV2 = ProfileCredentialsProvider.builder().profileFile(profileFile).profileName(profileName).build();
+    }
+
+    @Override
+    public AWSCredentials getCredentials() {
+        AwsCredentials awsCredentialsV2 = credentialsProviderV2.resolveCredentials();
+        if (awsCredentialsV2 instanceof AwsSessionCredentials) {
+            return new BasicSessionCredentials(awsCredentialsV2.accessKeyId(), awsCredentialsV2.secretAccessKey(), ((AwsSessionCredentials) awsCredentialsV2).sessionToken());
+        } else if (awsCredentialsV2 instanceof AwsBasicCredentials) {
+            return new BasicAWSCredentials(awsCredentialsV2.accessKeyId(), awsCredentialsV2.secretAccessKey());
+        } else {
+            throw new IllegalStateException("Unsupported V2 Credentials type " + awsCredentialsV2.getClass().getName());
+        }
+    }
+
+    @Override
+    public void refresh() {
+
+    }
+}
+
